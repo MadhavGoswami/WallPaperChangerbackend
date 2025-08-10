c9ab1ccf499b01b77e315c28a4bfc07e42934ddf
Madhav <goswamimadhav16@gmail.com>
2025-08-10 22:26:01 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/madhav/wallpaperchanger/wallpaperchanger/WallpaperchangerApplication.java
@@ -0,0 +1,13 @@
+package com.madhav.wallpaperchanger.wallpaperchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallpaperchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallpaperchangerApplication.class, args);
+	}
+
+}
